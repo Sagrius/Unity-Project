e30eb823b4bb94d4684c244c3ef164870e2ddc7b
Alexei <alexeibuic@gmail.com>
2024-03-10 21:17:54 +0200

    Basic Rotation and a bit of camera improvement

--- Y2 Final Project/Assets/Scriptes/PlayerMovment.cs
+++ Y2 Final Project/Assets/Scriptes/PlayerMovment.cs
@@ -6,6 +6,10 @@ using UnityEngine.InputSystem;
 public class PlayerMovment : MonoBehaviour
 {
     [SerializeField] PlayerInput PlayerController;
+    [SerializeField] Transform playerTransform;
+    [SerializeField] Transform playerArrow;
+    float rotationSpeed = 100;
+
 
     InputAction Move;
     int speed = 4;
@@ -17,6 +21,12 @@ public class PlayerMovment : MonoBehaviour
     private void Update()
     {
         MovePlayer();
+        RotatePlayer();
+        updateArrowDirection();
+    }
+    void updateArrowDirection()
+    {
+        playerTransform.eulerAngles = playerArrow.eulerAngles;
     }
 
     void MovePlayer()
@@ -24,5 +34,16 @@ public class PlayerMovment : MonoBehaviour
         Vector3 Direction = Move.ReadValue<Vector3>();
 
         transform.position += new Vector3(Direction.x, Direction.z, Direction.y) * speed * Time.deltaTime;
+    }
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(-Vector3.up*rotationSpeed*Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.E))
+        {
+        transform.Rotate(Vector3.up*rotationSpeed*Time.deltaTime);
+        }
     }
 }
